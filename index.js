8a50b1d840cbec77397e50600a99b5aae73c36cb
allysarh <allysa.rahagustiani@gmail.com>
2021-06-18 11:12:03 +0700

    tidy up

--- index.js
+++ index.js
@@ -33,8 +33,10 @@ db.getConnection((err, connection) => {
 
 // menggunakan express
 app.use(express.json())
-    .get('/', (req, res) => {
-        res.status(200).send(`<h2>${process.env.GRT}</h2>`)
+
+//ROUTING
+app.get('/', (req, res) => {
+        res.status(200).send(`<h1>${process.env.GRT}</h1>`)
     })
 app.use('/users', userRouter).get('/users', (req, res) => res.status(200).send('USER PAGE'))
 app.use('/products', productsRouter).get('/products', (req, res) => res.status(200).send('PRODUK PAGE'))
@@ -56,46 +58,4 @@ cert: fs.readFileSync('./ssl/ikeassl.cert')
 // LISTEN to port
 // app.listen(PORT, () => console.log("Server Running At:", PORT))
 
-
-//CATATAN DEPLOYMENT
-/**
- * Kalau menggunakan http: PORT 80
- * Kalau deploy front end: konfigurasi otomatis dengan web servernya
- * kalau backend: harus konfigurasi sendiri
- * kalau mengaktifkan fitur https: harus di konfigurasi di index.js utama
- * Harus ada sertifikat SSL
- * Membuat SSL dengan open SSL dari windows
- */
-
-/**
- * PROJECT MANAGEMENT
- * modul baru, belajar cara install webserver dan cara build servernya
- * pengelolaan jira
- * management: github
- * coba sampai penerapan sederhana dari CI/CD
- * final project: 2-3 minggu lagi
- * project management: mengelompokkan untuk sebuah proses management
- * mini final project
- * 
- * SERVER ssh --> supaya ga usah nunggu login server
- * pake puty --> dapet terminalnya aja
- * mobaexterm--> bisa liat direktori yang ada di server
- * 
- * PORVIDER server linode, digital ocean
- * server: PC kita tapi di internet --> OSnya linux
- * makanya harus terbiasa ddengan command line linuz
- * cari aja referensi command line linux
- * konfigurasionya mirip, tapi ada perbedaannya
- * 
- * mini server --> perusahaan yang membuat server sendiri
- * yang rumit -> maintenancenya
- * contoh intel ??
- * 
- * LINODE dan digital ocean: pure osnya saja
- * VPS: sudah disiapkan webservernya dan tools untuk tampilannya
- * Bahka ada c-panelnya
- * kalau hosting nanti hampir sama cuman kayak kita punya os windows dengan akun lain
- * (user1, user2, user3) --> dimana fiturnya terbatas
- * VPS: bisa mengakses akun rootnya (administrartornya)
- */
 
